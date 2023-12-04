9ed37c03fcfa25002ce7f70f688e237d15569646
Sonja Salmi <salmi.sonjakaroliina@gmail.com>
2023-12-04 13:20:15 +0200

    day4 part1

--- day4.py
+++ day4.py
@@ -1,1 +1,33 @@
 print("just another manic Monday")
+
+with open("syote_day4.txt") as file:
+    totalPoints = 0
+
+    for row in file:
+        nros = row.split(":")[1]
+        winners = nros.split("|")[0].split(" ")
+        myNros = nros.split("|")[1].split(" ")
+
+        rowPoints = 0
+        firstFound = False
+        for number in myNros:
+            number = number.strip()
+            if number not in winners:
+                continue
+            try:
+                int(number)
+                if not firstFound:
+                    firstFound = True
+                    rowPoints = 1
+                    # print(number + " is the first")
+                else:
+                    rowPoints *= 2
+                    # print(rowPoints)
+            except:
+                # print(number + " is not a number")
+                pass
+        
+        totalPoints += rowPoints
+
+
+    print(totalPoints)
